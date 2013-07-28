d4ca4ffbbca24dd0b9cb7aac825a7cd53d067033
fmeus <frank.meus@gmail.com>
2013-07-28 23:44:41 +0200

    Switch added to select Arduino or Digispark

--- adafruit_74hc595/adafruit_74hc595.ino
+++ adafruit_74hc595/adafruit_74hc595.ino
@@ -2,9 +2,20 @@
 Adafruit Arduino - Lesson 4. 8 LEDs and a Shift Register
  */
 
+// Set to 1 when using Digispark (uses different ports)
+#define BOARD_DIGISPARK 0
+
+#if BOARD_DIGISPARK == 0
+// Digispark
+int latchPin = 4;
+int clockPin = 5;
+int dataPin = 3;
+#else
+// Arduino/Freeduino
 int latchPin = 9;
 int clockPin = 10;
 int dataPin = 8;
+#endif
 
 byte leds = 0;
 
@@ -34,3 +45,4 @@ void updateShiftRegister()
   shiftOut(dataPin, clockPin, LSBFIRST, leds);
   digitalWrite(latchPin, HIGH);
 }
+
